0f63988cfd1eb4e8770ce893d85ac0998f3792f7
Jack <jackevans24@googlemail.com>
2020-03-08 20:54:32 +0000

    Implement shooting Add attack intervals

--- Assets/Scripts/Player/PlayerCombat.cs
+++ Assets/Scripts/Player/PlayerCombat.cs
@@ -2,41 +2,79 @@ using UnityEngine;
 
 public class PlayerCombat : MonoBehaviour
 {
-    public Animator animator;
-    public Transform attackPoint;
-    public LayerMask enemyLayers;
     public CharacterController2D controller;
-    public float attackRadius = 0.5f;
-    public float attackDamage = 20f;
-    public float attackRecoil = 0.2f;
+    public Animator animator;
+    public LayerMask enemyLayers;
+
+    public GameObject projectile;
+    public Transform shootPoint;
+    // public float shootRecoil = 0.1f;
+    public float shootInterval = 0.2f;
+    private float shootIntervalRemaining;
+    
+    public Transform blastPoint;
+    public float blastRadius = 0.5f;
+    public float blastDamage = 20f;
+    // public float blastRecoil = 0.2f;
+    public float blastInterval = 0.5f;
+    private float blastIntervalRemaining;
+
+    private void Start()
+    {
+        this.blastIntervalRemaining = this.blastInterval;
+    }
 
     void Update()
     {
-        if (Input.GetButtonDown("Blast"))
+        if (Input.GetButtonDown("Shoot") && CanAttack())
+        {
+            this.Shoot();
+        }
+        else if (Input.GetButtonDown("Blast") && CanAttack())
         {
             this.StartBlast();
         }
     }
 
+    private bool CanAttack() => blastIntervalRemaining <= 0 && shootIntervalRemaining <= 0;
+
+    private void FixedUpdate()
+    {
+        if (this.blastIntervalRemaining > 0)
+            this.blastIntervalRemaining -= Time.deltaTime;
+        if (this.shootIntervalRemaining > 0)
+            this.shootIntervalRemaining -= Time.deltaTime;
+    }
+
+    private void Shoot()
+    {
+        this.shootIntervalRemaining = this.shootInterval;
+        this.animator.SetTrigger("Shoot");
+        
+        var obj = Instantiate(projectile, shootPoint.position, shootPoint.rotation);
+        // controller.Recoil(shootRecoil, controller.m_FacingRight ? Vector2.left : Vector2.right);
+    }
+
     private void StartBlast()
     {
+        this.blastIntervalRemaining = this.blastInterval;
         this.animator.SetTrigger("Blast");
 
-        // controller.Recoil(attackRecoil, controller.m_FacingRight ? Vector2.left : Vector2.right);
+        // controller.Recoil(blastRecoil, controller.m_FacingRight ? Vector2.left : Vector2.right);
     }
 
     void Blast()
     {
-        var collisions = Physics2D.OverlapCircleAll(attackPoint.position, attackRadius, enemyLayers);
+        var collisions = Physics2D.OverlapCircleAll(blastPoint.position, blastRadius, enemyLayers);
 
         foreach (var enemy in collisions)
         {
-            enemy.GetComponent<Patrol>().TakeDamage(attackDamage);
+            enemy.GetComponent<Patrol>().TakeDamage(blastDamage);
         }
     }
 
     void OnDrawGizmosSelected()
     {
-        Gizmos.DrawWireSphere(attackPoint.position, attackRadius);
+        Gizmos.DrawWireSphere(blastPoint.position, blastRadius);
     }
 }
---
+++ Assets/Scripts/ProjectileController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileController : MonoBehaviour
+{
+    public float speed;
+    public float lifetime;
+    public float damage;
+
+    void Update()
+    {
+        if (lifetime < 0)
+            Destroy(this.gameObject);
+        
+        this.transform.Translate(this.speed, 0, 0);
+        lifetime -= Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Mobie")
+        {
+            other.GetComponent<Enemy>().TakeDamage(damage);
+            Destroy(this.gameObject);
+        }
+    }
+}
