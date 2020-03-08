8175cd8173795d4e2a00fe17ce9a905f15ce0b02
Jack <jackevans24@googlemail.com>
2020-03-08 13:36:07 +0000

    Show game over screen on death

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,8 +5,19 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject ui;
+    public GameObject gameOverCanvas;
+
     public Text murderValueText;
+    public Text gameOverMurderValueText;
+    public Text gameOverCommentText;
     private int mobieMurderCount;
+
+    private void Update()
+    {
+        if (this.gameOverCanvas.activeInHierarchy && Input.GetButton("Jump"))
+            RestartLevel();
+    }
 
     public static IEnumerator RestartInSeconds(float seconds)
     {
@@ -22,5 +33,30 @@ public class GameManager : MonoBehaviour
     {
         this.mobieMurderCount++;
         this.murderValueText.text = this.mobieMurderCount.ToString();
+        this.gameOverMurderValueText.text = this.mobieMurderCount.ToString();
+        this.SetCommentText();
+    }
+
+    private void SetCommentText()
+    {
+        var comment = string.Empty;
+
+        if (mobieMurderCount < 5) comment = "Pathetic";
+        else if (mobieMurderCount < 10) comment = "Weak";
+        else if (mobieMurderCount < 15) comment = "Eh";
+        else if (mobieMurderCount < 20) comment = "Could be better";
+        else if (mobieMurderCount < 30) comment = "Not bad";
+        else if (mobieMurderCount < 40) comment = "Keep trying";
+        else if (mobieMurderCount < 50) comment = "Getting there";
+        else if (mobieMurderCount < 75) comment = "That's more like it";
+        else comment = "Ok wow, didn't you get bored?";
+
+        this.gameOverCommentText.text = $"({comment})";
+    }
+
+    public void GameOver()
+    {
+        this.ui.SetActive(false);
+        this.gameOverCanvas.SetActive(true);
     }
 }
---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOver : MonoBehaviour
+{
+    public GameManager manager;
+    public Image background;
+    public Text gameOverText;
+    public Text youMurderedText;
+    public Text murderCountText;
+    public Text mobiesText;
+    public Text commentText;
+    public Text restartText;
+    private Text[] textObjects;
+
+    public float backgroundAlpha = 100f;
+    public float textShowInterval = 0.3f;
+    private bool showTextStarted;
+
+    void Start()
+    {
+        this.textObjects = new[] {
+            gameOverText,
+            youMurderedText,
+            murderCountText,
+            mobiesText,
+            commentText,
+            restartText
+        };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (background.color.a <= backgroundAlpha && !showTextStarted) {
+            showTextStarted = true;
+            StartCoroutine(ShowNextText(0));
+        }
+    }
+
+    IEnumerator ShowNextText(int i)
+    {
+        if (i >= textObjects.Length)
+            yield break;
+
+        yield return new WaitForSecondsRealtime(textShowInterval);
+
+        this.textObjects[i].enabled = true;
+        StartCoroutine(ShowNextText(++i));
+    }
+}
