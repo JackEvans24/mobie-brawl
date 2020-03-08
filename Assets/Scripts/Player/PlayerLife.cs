7e9a2decd179f172ca2759685cbaaf80b3e89b42
Jack <jackevans24@googlemail.com>
2020-03-08 11:49:32 +0000

    Add health text and update Add mobie murder count and update Add Sen font

--- Assets/Scripts/Player/PlayerLife.cs
+++ Assets/Scripts/Player/PlayerLife.cs
@@ -1,24 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerLife : MonoBehaviour
 {
     public Rigidbody2D rb;
     public Animator animator;
     public CharacterController2D controller;
+    public Text healthValueText;
     public float maxHealth = 100f;
     private float currentHealth;
 
     void Start()
     {
         this.currentHealth = this.maxHealth;
+        this.SetHealthText();
+
         this.animator.SetFloat("Health", this.currentHealth);
+    }
+
+    private void SetHealthText()
+    {
+        this.healthValueText.text = Mathf.Max(this.currentHealth, 0).ToString();
+        if (this.currentHealth <= 0)
+            this.healthValueText.color = new Color(212, 2, 2);
     }
 
     public void TakeDamage(float damage)
     {
         currentHealth -= damage;
+        this.SetHealthText();
+
         this.animator.SetTrigger("Hurt");
         this.animator.SetFloat("Health", this.currentHealth);
 
