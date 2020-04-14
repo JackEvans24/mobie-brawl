4ac3456277adce46a037f719ab868a0724e3b317
Jack <jackevans24@googlemail.com>
2020-04-14 15:51:28 +0100

    Add tutorial

---
+++ Assets/Scripts/Tutorial/ShowTextOnTrigger.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class ShowTextOnTrigger : MonoBehaviour
+{
+    public Rigidbody2D text;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+            this.text.bodyType = RigidbodyType2D.Dynamic;
+    }
+}
